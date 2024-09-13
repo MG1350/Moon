2983253b9e54c23b9bcd8a096391dba352ea503f
MG1350 <114371390+MG1350@users.noreply.github.com>
2024-09-13 14:47:44 -0400

    Update moonCoding.java

--- moonCoding.java
+++ moonCoding.java
@@ -7,12 +7,21 @@ public class moonCoding
         double kmHour = kmSec * 3600;
         double time = moonDist / kmHour;
         System.out.println("Travel Without Gravity In Hours: " + time);
+        
         double hSpeed = 2000;
         double vSpeed = 3000;
         double total = hSpeed*hSpeed + vSpeed*vSpeed;
         double totalSpeed = Math.sqrt(total);
-        System.out.println("Totla Speed In Meter/Sec: " + totalSpeed);
+        System.out.println("Total Speed In Meter/Sec: " + totalSpeed);
 
-
+        double radius = 1731 * 1000;
+        double sArea = 4*3.14*radius*radius;
+        double sVolume = sArea * 0.001;
+        double density = 3000;
+        double regolith = sVolume * density / 1000;
+        double water = 0.25 * regolith;
+        System.out.println("A: " + sVolume);
+        System.out.println("B: " + regolith);
+        System.out.println("C: " + water);
     }
 }
